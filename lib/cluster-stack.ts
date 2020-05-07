703a61a825d17dee96d7c7440d5afb849e72800b
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-05-07 16:19:40 +0900

    PhysicalName added to clusterstack

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as eks from '@aws-cdk/aws-eks';
+import { PhysicalName } from '@aws-cdk/core';
 
 export class ClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
