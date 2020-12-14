9c8df537af95fb8858e738abd6aa11be1e5801c1
Jiwon Yeom <jiwony@amazon.com>
2020-12-14 13:47:44 +0900

    read-file updated

--- utils/read-file.ts
+++ utils/read-file.ts
@@ -1,26 +1,26 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as eks from '@aws-cdk/aws-eks';
+import {KubernetesManifest} from "@aws-cdk/aws-eks/lib/k8s-manifest";
 
 
 export function readYamlFromDir(dir: string, cluster: eks.Cluster) {
-    fs.readdir(dir, 'utf8', (err, files) => {
-      if (files!=undefined) {
-        files.forEach((file) => {
-        if (file.split('.').pop()=='yaml') {
-          fs.readFile(dir+file, 'utf8', (err, data) => {
-            if (data!=undefined) {
-              let i = 0;
-              yaml.loadAll(data).forEach((item) => {
-                cluster.addManifest(file.substr(0,file.length-5)+i, item);
-                i++;
-              })
-            }
-          })
-        }
-      })} else {
-        console.log(`${dir} is empty`);
+  let previousResource: KubernetesManifest;
+  fs.readdirSync(dir, "utf8").forEach(file => {
+    if (file != undefined && file.split('.').pop() == 'yaml') {
+      let data = fs.readFileSync(dir + file, 'utf8');
+      if (data != undefined) {
+        let i = 0;
+        yaml.loadAll(data).forEach((item) => {
+          const resource = cluster.addManifest(file.substr(0, file.length - 5) + i, item);
+          // @ts-ignore
+          if (previousResource != undefined) {
+            resource.node.addDependency(previousResource)
+          }
+          previousResource = resource;
+          i++;
+        })
       }
-        
-      })
+    }
+  });
 }
